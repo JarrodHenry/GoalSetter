19aae7dacca8cf52c631ef1a0035d3ed824c1d11
Jarrod Henry <jarrod.henry@sftsrc.com>
2017-01-04 15:53:34 -0800

    Adding in draggable components

--- app/components/Body.jsx
+++ app/components/Body.jsx
@@ -3,6 +3,8 @@ import { GoalList } from './GoalList'
 import GoalNumber from './GoalNumber'
 import GoalHabit from './GoalHabit'
 import AddCardComponent from './AddCardComponent'
+import Draggable from 'react-draggable';
+import {DraggableCore} from 'react-draggable';
 
 export default class Body extends Component {
   constructor(props) {
@@ -12,27 +14,42 @@ export default class Body extends Component {
   render() {
     return (
         <div className="container">
+            <Draggable grid={[20,20]}>
             <div className="col-xs-6 col-md-4">
                 <GoalList />
               </div>
+            </Draggable>
+            <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalList />
               </div>
+              </Draggable>
+              <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalList />
               </div>
+              </Draggable>
+              <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalHabit />
               </div>
+              </Draggable>
+              <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalHabit />
               </div>
+              </Draggable>
+              <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalNumber />
               </div>
+              </Draggable>
+              <Draggable grid={[20,20]}>
               <div className="col-xs-6 col-md-4">
                 <GoalNumber />
               </div>
+              </Draggable>
+
               <div className="col-xs-6 col-md-4">
                 <AddCardComponent />
               </div>
